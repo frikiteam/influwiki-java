4a1897deb4ce1103fab55a88b28343d7d66b696a
Yesid Rangel <yety93@hotmail.com>
2021-10-14 22:54:03 -0500

    Agregada la funcionalidad de alacenar los datos del nuevo influwiki

--- influwiki/src/main/java/co/frikiteam/influwiki/Dao/UsuarioDao.java
+++ influwiki/src/main/java/co/frikiteam/influwiki/Dao/UsuarioDao.java
@@ -3,10 +3,13 @@ package co.frikiteam.influwiki.Dao;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+import java.time.LocalDate;
 import java.util.ArrayList;
+import java.util.Date;
 
 import co.frikiteam.influwiki.model.ConexionPrincipalBD;
 import co.frikiteam.influwiki.model.Usuario;
+import javassist.expr.NewArray;
 
 public class UsuarioDao {
 
@@ -14,32 +17,35 @@ public class UsuarioDao {
 	static ConexionPrincipalBD conBD = null;
 
 	public boolean almacenarUsuario(Usuario usuario) {
-		
+
 		Connection conexionBD = null;
 		boolean ingresarDatos = true;
-		
+
 		if (conexionBD == null) {
 			conexionBD = conBD.obetenerConexion();
 		}
-		
+
 		// prpStmt se encarga de definir unaconsulta y ejecutarla
-		
+
 		PreparedStatement prpStmt = null;
-		
+
 		try {
-			String sentenciSQL ="INSERT INTO influwiki.usuarios"
-					+ "(nombres, apellidos, genero, apodo, email, contrasena, id_perfil, contenido_principal, fecha_registro, actulizado, id_area) "
-					+ " VALUES" + "(?,?,?,?,?,?,?,?,?,?,?,?)";
-			
-			
+			String sentenciSQL = "INSERT INTO usuarios"
+					+ "( nombres, apellidos, genero, apodo, email, contrasena, id_perfil, contenido_principal, fecha_registro, actulizado, id_area) "
+					+ " VALUES" + "(?,?,?,?,?,?,?,?,?,?,?)";
+
 			/**
-			 * INSERT INTO `usuarios` (`id`, `nombres`, `apellidos`, `genero`, `apodo`, `email`, `contrasena`, `id_perfil`, `contenido_principal`, `fecha_registro`, `actulizado`, `id_area`) 
-			 * VALUES (NULL, 'Maria', 'Garcia', 'Femenino', 'alejandra', 'Maria.garcia@hotmail.com', '12345678', '1', 'Tutora en matematicas', '2021-10-01', '2021-10-12', '2');
+			 * INSERT INTO `usuarios` (`id`, `nombres`, `apellidos`, `genero`, `apodo`,
+			 * `email`, `contrasena`, `id_perfil`, `contenido_principal`, `fecha_registro`,
+			 * `actulizado`, `id_area`) VALUES (NULL, 'Maria', 'Garcia', 'Femenino',
+			 * 'alejandra', 'Maria.garcia@hotmail.com', '12345678', '1', 'Tutora en
+			 * matematicas', '2021-10-01', '2021-10-12', '2');
 			 */
-			
-			
-			//Asociamos cada uno de los elementos del objeto Usuario
-			//a cada una de los Signos ? de pregunta
+
+			prpStmt = conexionBD.prepareStatement(sentenciSQL);
+
+			// Asociamos cada uno de los elementos del objeto Usuario
+			// a cada una de los Signos ? de pregunta
 			prpStmt.setString(1, usuario.getNombres());
 			prpStmt.setString(2, usuario.getApellidos());
 			prpStmt.setString(3, usuario.getGenero());
@@ -51,42 +57,71 @@ public class UsuarioDao {
 			prpStmt.setString(9, usuario.getFecha_registro());
 			prpStmt.setString(10, usuario.getActulizado());
 			prpStmt.setInt(11, usuario.getId_area());
-			
+
 			prpStmt.execute();
-			
+
 		} catch (Exception e) {
-			System.out.println("Se presento error al crear el ingreso a la tabla libros " + e);
+			System.out.println("Se presento error al crear el ingreso a la tabla usuarios " + e);
 			ingresarDatos = false;
-		}finally {
+		} finally {
 			try {
 				conexionBD.close();
 				conBD.cerrarConexion();
-				
+
 			} catch (Exception errorException) {
 				System.out.println("Se presento error al cerrar la conexion a la base de datos " + errorException);
 			}
 		}
-		
+
 		return ingresarDatos;
 	}
-	
-	public static ArrayList getInflusuarios() {
+
+	/**
+	 * Metodo que retorna la lista de usuarios influwiki
+	 * @return
+	 */
+	public ArrayList<Usuario> getInflusuarios() {
 		Connection conectBD = null;
 		PreparedStatement prpStmt = null;
 		ResultSet rstUsuario = null;
-		ArrayList Usuarios = new ArrayList();
+		ArrayList <Usuario>listaUsuarios = new ArrayList<Usuario>();
+		Usuario usuarioInfluwili;
 		
 		try {
+			//Consulta sql donde se traen todos los resgistros de la tabla Usuarios
 			String strStm = null;
-			int intColumnas = 0;
-			int intFilas = 0;
 			conectBD = ConexionPrincipalBD.obetenerConexion();
+			strStm = "SELECT id,nombres, apellidos, genero, apodo, email, contrasena, id_perfil, contenido_principal, fecha_registro, actulizado, id_area, "
+					+ "FROM usuarios ";
+			//
+			rstUsuario = prpStmt.getGeneratedKeys();
+			
+			while(rstUsuario.next()) {
+				usuarioInfluwili = new Usuario();
+				usuarioInfluwili.setId(Integer.parseInt(rstUsuario.getString("id")));
+				usuarioInfluwili.setNombres(rstUsuario.getNString("nombres"));
+				usuarioInfluwili.setApellidos(rstUsuario.getNString("apellidos"));
+				usuarioInfluwili.setGenero(rstUsuario.getNString("genero"));
+				usuarioInfluwili.setApodo(rstUsuario.getNString("apodo"));
+				usuarioInfluwili.setCorreo(rstUsuario.getNString("email"));
+				usuarioInfluwili.setContrasena(rstUsuario.getNString("contrasena"));
+				usuarioInfluwili.setId_perfil(Integer.parseInt(rstUsuario.getNString("id_perfil")));
+				usuarioInfluwili.setContenido(rstUsuario.getNString("contenido_principal"));
+				usuarioInfluwili.setFecha_registro(rstUsuario.getNString("fecha_registro"));
+				usuarioInfluwili.setActulizado(rstUsuario.getNString("actualizado"));
+				usuarioInfluwili.setId_area(Integer.parseInt(rstUsuario.getNString("id_area")));
+				
+				listaUsuarios.add(usuarioInfluwili);
+				
+			}
+			rstUsuario.close();
 			
 			
-			
+						
 		}catch (Exception e) {
 			System.out.println("Se presento un error al reallizar la consulta a la BD " + e);
 		}
+		return listaUsuarios;
 	}
 
 }
--- influwiki/src/main/java/co/frikiteam/influwiki/controller/UsuarioBean.java
+++ influwiki/src/main/java/co/frikiteam/influwiki/controller/UsuarioBean.java
@@ -1,6 +1,9 @@
 package co.frikiteam.influwiki.controller;
 
 import java.io.Serializable;
+import java.text.SimpleDateFormat;
+import java.time.LocalDate;
+import java.util.ArrayList;
 import java.util.LinkedHashMap;
 import java.util.Map;
 
@@ -10,6 +13,7 @@ import javax.faces.bean.SessionScoped;
 import javax.faces.context.FacesContext;
 
 import co.frikiteam.influwiki.Dao.UsuarioDao;
+import co.frikiteam.influwiki.model.Usuario;
 
 /**
  * Clase bean de usuario
@@ -40,7 +44,7 @@ public class UsuarioBean implements Serializable {
 
 	private String contrasena;
 
-	private String id_perfil; // hace referencia si es un influencer o Administrador
+	private boolean id_perfil = true; // hace referencia si es un influencer o Administrador
 
 	private String contenido;
 
@@ -51,23 +55,18 @@ public class UsuarioBean implements Serializable {
 	private int id_area; // hace referencia al tipo detematica
 
 	private String mensajeSaludo;
-	
+
 	private boolean terminosCondiciones;
-	
-	//Variable selecionada de forma permamente para pefil influwiki
-	
+
+	// Variable selecionada de forma permamente para pefil influwiki
 
 	public UsuarioBean() {
 
 	}
-	
+
 	/**
 	 * Función encargada de activar el check en id_influwiki
 	 */
-    public void init() {
-    	id_perfil = "1";
-    }
-
 
 	public long getId() {
 		return id;
@@ -125,13 +124,12 @@ public class UsuarioBean implements Serializable {
 		this.contrasena = contrasena;
 	}
 
-	public String getId_perfil() {
+	public boolean isId_perfil() {
 		return id_perfil;
 	}
 
-	public void setId_perfil(String id_perfil) {
+	public void setId_perfil(boolean id_perfil) {
 		this.id_perfil = id_perfil;
-		
 	}
 
 	public String getContenido() {
@@ -178,11 +176,10 @@ public class UsuarioBean implements Serializable {
 		this.mensajeSaludo = mensajeSaludo;
 	}
 
-	
 	/*
 	 * Metodos encargados de realixar las distintas validaciones
 	 */
-	
+
 	/*
 	 * Metodo encargado de validar el inicio de sesion
 	 */
@@ -225,33 +222,114 @@ public class UsuarioBean implements Serializable {
 	public void setTerminosCondiciones(boolean terminosCondiciones) {
 		this.terminosCondiciones = terminosCondiciones;
 	}
-	
+
 	/**
 	 * 
 	 */
 	public void mostrarDatos() {
 		FacesContext.getCurrentInstance().addMessage(null, new FacesMessage("Welcome " + terminosCondiciones));
+
 	}
-
-
 
 	/*
 	 * Metodos que ejecutam sql
 	 */
 
-	//Creación del obejeto Usuario
+	// Creación del obejeto Usuario
 	static UsuarioDao usuarioDAO = new UsuarioDao();
-	
-	//Creación del Mapeo
+
+	// Creación del Mapeo
 	private static Map<String, Object> mapUsuario;
-	
+
 	static {
 		mapUsuario = new LinkedHashMap<String, Object>();
-		//mapUsuario = 
+		// mapUsuario =
+	}
+
+	// Arreglo que recibe las datos del usuarioDAO
+
+	// Asignar Perfil influwiki obligatorio
+	public int asignarPerfilInfluwiki() {
+		id_perfil = true;
+		int perfilInfluwiki = 0;
+
+		if (id_perfil) {
+			perfilInfluwiki = 1;
+
+		}
+		return perfilInfluwiki;
 	}
 	
-	//Arreglo que recibe las datos del usuarioDAO
+
+	/**
+	 * Metodo que guarda la información en base de datos
+	 */
+	public String guardarUsuarioForm() {
+		boolean conexionExitosa;
+		String Url = "login_form.xhtm";
+
+		// Obtención de fecha
+		String fechaActual = LocalDate.now().toString();
+
+		// SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+		fecha_registro = fechaActual;
+		actulizado = fechaActual;
+
+		// clase que contiene el metodo de guardar en bd
+		UsuarioDao usuarioDao = new UsuarioDao();
+		// Clase de modelo
+		Usuario usuario = new Usuario();
+
+		// Asiagnación de datos al modelo de usuario
+		usuario.setNombres(nombres);
+		usuario.setApellidos(apellidos);
+		;
+		usuario.setApodo(apodo);
+		usuario.setCorreo(correo);
+		usuario.setContrasena(contrasena);
+		usuario.setGenero(genero);
+		usuario.setId_perfil(asignarPerfilInfluwiki());
+		usuario.setId_area(id_area);
+		usuario.setContenido(contenido);
+		usuario.setFecha_registro(fecha_registro);
+		usuario.setActulizado(actulizado);
+
+		System.out.println(usuario);
+
+		// Ejecuación de inserción de datos en bd
+		//conexionExitosa = usuarioDao.almacenarUsuario(usuario);
+		conexionExitosa = true;
+		
+
+		// se detiene 4 segundos antes de seguir
+		
+			if (conexionExitosa) {
+				FacesContext.getCurrentInstance().addMessage(null,
+						new FacesMessage("Se esta validando el " + "Resgistros para " + nombres));
+
+			}
+			
+			/*
+			 * try { Thread.sleep(4 * 1000); } catch (Exception e) { System.out.println(e);
+			 * 
+			 * }
+			 */
+			 ConsultarUsariosInfluwiki();
+		return Url;
+
+	}
 	
 	
-	
+	public void ConsultarUsariosInfluwiki() {
+		int tamanioArray;
+		UsuarioDao usuarios = new UsuarioDao();
+		
+		ArrayList<Usuario>listaCompleta =  usuarios.getInflusuarios();
+		
+
+		System.out.println(listaCompleta);
+		
+		
+	}
+
 }
